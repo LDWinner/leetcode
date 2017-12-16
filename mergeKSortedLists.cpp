78e636d7954514c0ba4934d152507abe3e674bb0
ldwinner <wliu92@163.com>
2017-12-16 09:54:54 +0800

    add mergeKSortedLists.cpp

---
+++ mergeKSortedLists.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+    }
+    void mergeKListsDFS(vector<ListNode*>& lists,int level,ListNode* result)
+    {
+    }
+};

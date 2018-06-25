72ee2bcb6a2bce001a2e9e491ed6113146547d2d
ldwinner <ldwinner@gmail.com>
2018-06-25 20:06:21 +0800

    update solution

--- DivideTwoIntegers.cpp
+++ DivideTwoIntegers.cpp
@@ -1,32 +1,36 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        int8_t flag1 = 0;
-        int8_t flag2 = 0;
-        if(dividend < 0)
-        {
-            dividend = -dividend;
-            flag1 = 1;
-        }
-        if(divisor < 0)
-        {
-            divisor = -divisor;
-            flag2 = 1;
-        }
+        if(divisor == -1 && dividend == (numeric_limits<int>::min)())
+            return (numeric_limits<int>::max)();
+        if(divisor == 1)
+            return dividend;
+        unsigned long long div = abs((long long)dividend);
+        unsigned long long dir = abs((long long)divisor);
 
-        int tmp = divisor;
-        int result = 0;
+        unsigned long long tmp = dir;
+        long long result = 0;
 
-        while(tmp <= dividend)
+        while(tmp <= div)
         {
-            tmp += divisor;
             ++result;
+            tmp+=dir;
         }
 
-        if(flag1 ^ flag2)
-        {
-            result = -result;
-        }
-        return result;
+        if((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
+            return 0-result;
+        else
+            return result;
     }
 };
+
+int main()
+{
+    Solution s;
+    cout << s.divide(2147483647,2) << endl;;
+    return 0;
+}
